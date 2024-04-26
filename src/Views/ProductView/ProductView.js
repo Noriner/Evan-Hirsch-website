b7273e8e597085621927e56b6853780758b921d8
Noriner <59485213+Noriner@users.noreply.github.com>
2024-04-26 13:00:16 +0200

    Product&Service
    All inputs for product and service site

--- src/Views/ProductView/ProductView.js
+++ src/Views/ProductView/ProductView.js
@@ -3,6 +3,10 @@ import SubPageFooter from "../../Components/Footer/SubPageFooter/SubPageFooter";
 import styles from "./ProductView.module.scss";
 import phone from "../../Pictures/genericphone.png";
 import movie from "../../Videos/Phonevideo.mov";
+import bravemind from "../../Pictures/bravemind.png";
+import hotel from "../../Pictures/hotelgamescene.png";
+import mail from "../../Pictures/mail.png";
+import Biochill from "../../Pictures/Biochill.png";
 
 const  ProductView = () => {
     return(
@@ -25,12 +29,41 @@ const  ProductView = () => {
                         </div>
                     </div>
                     <div id={styles.title2} className={styles.title}>BraveMind MST</div>
+                    <div id={styles.text2} className={styles.text}>
+                        <div className={styles.imgholder}><img src={bravemind} alt="Video game scene" /></div>
+                        <div className={styles.textbetween}>
+                            Working with USC & Dept of Veterans Affairs (VA), 
+                            designed a new version of Bravemind (VR exposure therapy), 
+                            to help Military Sex Trauma survivors overcome 
+                            their Post-Traumatic Stress Disorder (PTSD).
+                        </div>
+                        <div className={styles.imgholder}><img src={hotel} alt="Hotel game scene" /></div>
+                    </div>
                     <div id={styles.title3} className={styles.title}>CounterNet</div>
+                    <div id={styles.text3} className={styles.text}>
+                        <div>
+                            Designed and produced a training simulation game with USC for 
+                            the US Naval Postgraduate School to train  operators and 
+                            policymakers to identify and disrupt internet terrorist recruitment. 
+                        </div>
+                        <div className={styles.imgholder}><img src={mail} alt="Mail text" /></div>
+                    </div>
                     <div id={styles.title4} className={styles.title}>
                         <div>BioChill</div>
-                        <div>(Dutch National Police)</div>
+                        <div className={styles.subtitle}>(Dutch National Police)</div>
                     </div>
-
+                    <div id={styles.text4} className={styles.text}>
+                        <div className={styles.imgholder}><img src={Biochill} alt="Policeman" /></div>
+                        <div>
+                            Working with neuroscience and psychology researchers, 
+                            lead design of a biofeedback VR training system to investigate officers’ 
+                            ability to improve decision making in high-stress, 
+                            high-stakes situations (currently in RCTs).
+                        </div>
+                    </div>
+                    <div id={styles.breaker1} className={styles.breakerbox}><div className={styles.breaker}></div></div>
+                    <div id={styles.breaker2} className={styles.breakerbox}><div className={styles.breaker}></div></div>
+                    <div id={styles.breaker3} className={styles.breakerbox}><div className={styles.breaker}></div></div>
                 </div>
             </div>
             
