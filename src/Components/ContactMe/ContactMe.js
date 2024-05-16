96635f98467da59f3acbee4590a375763af1c84f
Noriner <59485213+Noriner@users.noreply.github.com>
2024-05-16 11:34:34 +0200

    Mobile design - v0.1

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./ContactMe.module.scss";
+import mail from "../../Pictures/mailicon.png";
 
 function ContactMe(){
     return(
         <>
             <div className={styles.wrapper}>
-                <a href="mailto:ev@evhirsch.com"><div>Get in touch</div></a>
+                <a className={styles.mail} href="mailto:ev@evhirsch.com"><div>Get in touch</div></a>
+                <div className={styles.mailicon}><a href="mailto:ev@evhirsch.com"><img src={mail} alt="" /></a></div>
                 <div className={styles.bordertop}></div>
                 <div className={styles.borderright}></div>
             </div>
